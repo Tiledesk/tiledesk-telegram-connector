8d67e5838bf60136ae1b9f9db24879eebcfdd210
Gabriele Panico <gabriele.panico@gmail.com>
2025-10-29 09:51:37 +0100

    version added: ### 0.1.14

--- telegramRoute/publish.sh
+++ telegramRoute/publish.sh
@@ -1,13 +1,8 @@
 #npm version patch
 version=`node -e 'console.log(require("./package.json").version)'`
-version_server=`node -e 'console.log(require("./telegramRoute/package.json").version)'`
 echo "version $version"
 
 ## Update package-lock.json
-npm install
-
-## Update voiceRoute/package-lock.json
-cd ./telegramRoute
 npm install
 
 cd ..
@@ -20,12 +15,10 @@ if [ "$version" != "" ]; then
     git tag -a "$version" -m "`git log -1 --format=%s`"
     echo "Created a new tag, $version"
     git push --tags
-    cd ./telegramRoute
     npm publish --access public
 fi
 
 echo "***************************************************************************"
-echo "  Deployed: @tiledesk/tiledesk-voice-twilio-connector:$version_server"
 echo "       Tagged: tiledesk/tiledesk-telegram-connector:$version"
 echo "***************************************************************************"
 
